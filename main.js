83c7d555f971257d936a35eccdd71a8b818b69f3
Jaheedgamer <88264622+Jaheedgamer@users.noreply.github.com>
2021-12-04 15:55:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -38,3 +38,92 @@ function block_image(get_image){
         canvas.add( block_img);
     });
     }
+
+    window.addEventListener("keydown" , my_keydown);
+
+    function my_keydown(e){
+        keypressed = e.keyCode;
+        console.log(keypressed);
+
+        if(e.shiftKey == true && keypressed == "80"){
+            console.log("p and shift pressde together");
+            block_width = block_width+10;
+            block_height = block_height+10;
+            document.getElementById("current_width").innerHTML = block_width;
+            document.getElementById("current_height").innerHTML = block_height;
+        }
+
+        if(e.shiftKey && keypressed == "78"){
+          block_width = block_width-10;
+          block_height = block_height-10;
+          document.getElementById("current_width").innerHTML = block_width;
+            document.getElementById("current_height").innerHTML = block_height;
+        }
+
+        if(keypressed == "38"){
+              up();
+              console.log("up");
+        }
+
+        if(keypressed == "40"){
+            down();
+            console.log("down");
+      }
+
+      if(keypressed == "37"){
+        left();
+        console.log("left");
+  }
+
+  if(keypressed == "39"){
+    right();
+    console.log("right");
+}
+
+if(keypressed == "67"){
+    block_image("cloud.jpg")
+    console.log("c");
+}
+
+if(keypressed == "68"){
+    block_image("dark_green.png")
+    console.log("d");
+}
+
+if(keypressed == "71"){
+    block_image("ground.png")
+    console.log("g");
+}
+
+if(keypressed == "76"){
+    block_image("light_green.png")
+    console.log("l");
+}
+
+if(keypressed == "82"){
+    block_image("roof.jpg")
+    console.log("r");
+}
+
+if(keypressed == "84"){
+    block_image("trunk.jpg")
+    console.log("t");
+}
+
+if(keypressed == "85"){
+    block_image("unique.png")
+    console.log("u");
+}
+
+if(keypressed == "87"){
+    block_image("wall.jpg")
+    console.log("w");
+}
+
+if(keypressed == "89"){
+    block_image("yellow_wall.png")
+    console.log("y");
+}
+    }
+
+          
