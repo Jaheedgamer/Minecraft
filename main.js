90f57ad6314ff694661af9121f59e45c0456055f
Jaheedgamer <88264622+Jaheedgamer@users.noreply.github.com>
2021-12-25 15:45:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -127,3 +127,34 @@ if(keypressed == "89"){
     }
 
           
+function up(){
+    if(player_y >=0){
+        player_y = player_y - block_height;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down(){
+    if(player_y <=500){
+        player_y = player_y + block_height;
+        canvas.remove(player_object);
+          player_update();
+    }
+}
+
+function left(){
+    if(player_x >=0){
+        player_x = player_x - block_width;
+        canvas.remove(player_object);
+          player_update();
+    }
+}
+
+function right(){
+    if(player_x <=850){
+        player_x = player_x + block_width;
+        canvas.remove(player_object);
+          player_update();
+    }
+}
